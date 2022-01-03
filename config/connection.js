23c8378a71334406eec1f102c9ccab95610940e0
blanebussey <blanebussey@gmail.com>
2022-01-03 17:56:43 -0600

    meh

---
+++ config/connection.js
@@ -0,0 +1,14 @@
+//import the sequelizer constructor from the library
+const Sequelize = require('sequelize');
+
+require('dotenv').config();
+
+
+//create connection to our database, pass in your mysql infor for username and password 
+const sequelize = new Sequelize('just_tech_news_db', 'username', 'password', {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+});
+
+module.exports = sequelize
