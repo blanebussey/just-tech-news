08f1283fa0d4fccbb21aada00a49d9cb328c0c4b
blanebussey <blanebussey@gmail.com>
2022-01-03 20:30:13 -0600

    whatever

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 
 //create connection to our database, pass in your mysql infor for username and password 
-const sequelize = new Sequelize('just_tech_news_db', 'username', 'password', {
+const sequelize = new Sequelize('just_tech_news_db', 'root', '', {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
